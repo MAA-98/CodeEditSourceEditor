b8ef5573a42123238cde180e0706fda1ee244821
MAA-98 <marek.a.kurczynski@gmail.com>
2026-04-18 22:25:56 +0100

    Fixing package.swift type error.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
             exact: "0.1.20"
         ),
         // CodeEditSymbols
-        .package(url: "https://github.com/MAA-98/CodeEditSymbols.git", branch: main)
+        .package(url: "https://github.com/MAA-98/CodeEditSymbols.git", branch: "main")
         //.package(
         //    url: "https://github.com/CodeEditApp/CodeEditSymbols.git",
         //    exact: "0.2.3"
